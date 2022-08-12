ff04383e84e6dd5c16a8262f572bbe362019947f
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-08-12 11:41:35 -0300

    Apply configurations to worker random port and register on eureka server

--- hr-payroll/src/main/java/com/rddev/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/rddev/hrpayroll/HrPayrollApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-//@LoadBalancerClient(name = "hr-worker", configuration = WorkerLoadBalanceConfiguration.class)
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication

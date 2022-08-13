1eaa70c3ea8b68faf299b2acd4a01ad70500c0a5
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-08-13 09:30:36 -0300

    Initial gateway api server

---
+++ hr-gateway-server/src/main/java/com/rddev/hrgatewayserver/HrGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.rddev.hrgatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrGatewayServerApplication.class, args);
+	}
+
+}
---
+++ hr-gateway-server/src/test/java/com/rddev/hrgatewayserver/HrGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rddev.hrgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

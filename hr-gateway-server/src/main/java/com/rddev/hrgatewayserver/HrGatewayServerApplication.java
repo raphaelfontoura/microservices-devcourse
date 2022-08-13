59e4f77605372a5f03b490d1ac64ee6f7a8e487e
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-08-13 11:33:07 -0300

    Configure api gateway

--- hr-gateway-server/src/main/java/com/rddev/hrgatewayserver/HrGatewayServerApplication.java
+++ hr-gateway-server/src/main/java/com/rddev/hrgatewayserver/HrGatewayServerApplication.java
@@ -2,7 +2,10 @@ package com.rddev.hrgatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class HrGatewayServerApplication {
 

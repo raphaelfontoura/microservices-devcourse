10e9c62389e3adf3829a26f3e9f648f3fd700313
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-08-14 16:26:37 -0300

    hr-oauth first commit

---
+++ hr-oauth/src/test/java/com/rddev/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rddev.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
